ed5ba5f1e395108e35e4028a936677d7e919aa1f
AndresNoeg <a.23308051281184@cbtis128.edu.mx>
2024-09-23 20:48:26 -0600

    Practica 1 multiplicacion de 3 numeros

---
+++ Multiplicacion.py
@@ -0,0 +1,15 @@
+print (" ")
+print ("Andres Noe Gomez Garcia Multiplicacion de 3 numeros ")
+print(" ")
+
+#Pide 3 valores al usuario
+num1=int(input("ingrese el primer valor: "))
+num2=int(input("ingrese el segundo valor:"))
+num3=int(input("ingrese el tercer valor: "))
+
+#Formula para realizar la multiplicacion de 3 valores
+r=num1*num2*num3
+
+#imprime el resultado de la multiplicacion
+print("El resultado de la operacion es", r)
+print(" ")
